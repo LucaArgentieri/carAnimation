f782f52afb64434570a1e903bc030a3df2b4adfb
LucaArgentieri <Lucaargentieri-98@hotmail.it>
2021-02-16 17:17:59 +0100

    Update

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ class Car {
     constructor() {
         this.angle = 0
         this.speed = 10
+        this.border = document.querySelector('.container')
         this.btn = document.querySelector('.btn')
         this.el = document.querySelector('.car')
+        this.keysPressed = {};
     }
     init() {
         window.addEventListener('keydown', evt => {
+            this.keysPressed[evt.key] = true;
             if (evt.key === 'ArrowRight') {
                 this.angle = 0
                 this.speed++
@@ -48,7 +51,41 @@ class Car {
                     rotation: 180
                 })
             }
+            // Combo di tasti
+            if (this.keysPressed['ArrowLeft'] && evt.key == 'ArrowDown' || this.keysPressed['ArrowDown'] && evt.key == 'ArrowLeft') {
+                this.angle = -225
+                console.log('left + down')
+                gsap.to(this.el, {
+                    rotation: -225
+                })
+                moveDownUpLeft()
+            }
+            if (this.keysPressed['ArrowRight'] && evt.key == 'ArrowDown' || this.keysPressed['ArrowDown'] && evt.key == 'ArrowRight') {
+                this.angle = 45
+                gsap.to(this.el, {
+                    rotation: 140
+                })
+                moveDownUpRight()
+            }
+            if (this.keysPressed['ArrowLeft'] && evt.key == 'ArrowUp' || this.keysPressed['ArrowUp'] && evt.key == 'ArrowLeft') {
+                this.angle = 225
+                gsap.to(this.el, {
+                    rotation: 320
+                })
+                moveDownUpLeft()
+
+            }
+            if (this.keysPressed['ArrowRight'] && evt.key == 'ArrowUp' || this.keysPressed['ArrowUp'] && evt.key == 'ArrowRight') {
+                this.angle = -45
+                gsap.to(this.el, {
+                    rotation: 45
+                })
+                moveDownUpRight()
+
+            }
         })
+
+
 
         window.addEventListener('keyup', evt => {
             this.speed = 10
@@ -79,6 +116,20 @@ class Car {
             })
 
         }
+        const moveDownUpRight = () => {
+            gsap.to(this.el, {
+                x: "+=" + (this.speed * Math.cos(this.angle * Math.PI / 180))
+            })
+        }
+        const moveDownUpLeft = () => {
+            gsap.to(this.el, {
+                x: "+=" + (this.speed * Math.cos(this.angle * Math.PI / 180))
+            })
+        }
+
+        document.addEventListener('keyup', (event) => {
+            delete this.keysPressed[event.key];
+        });
     }
 
 
@@ -86,5 +137,12 @@ class Car {
 
 
 const macchina = new Car
-macchina.init()
+const startAnimation = () => {
+    macchina.init()
+}
+window.requestAnimationFrame(startAnimation)
+
+document.addEventListener('keydown', (event) => {
+
+});
 
